882a6425f12f61859c6d141eb79276202c5e4083
jimmikedave <62654421+jimmikedave@users.noreply.github.com>
2020-12-14 12:51:59 -0500

    Add index project components

--- s4v1/app.js
+++ s4v1/app.js
@@ -22,12 +22,44 @@ const cards = [
     },
   ]; 
 
+  // Display our data: done
+  // On click: flip cards back and forth: done
+  // Get info for new cards from the user: done
+    // Create data properties for front and back of new card
+    // Bind properties to the form inputs of v model
+  // Add new card when user hits enter or clicks button
+    // create a method
+        // creates a new card object containing new card info
+        // pushes that card object into the cards array
+  // Delete cards
+  // Animate card flip
+  // Display an error message if form fields are blank
+
   new Vue({
     el: '#flashcard-app',
     data: {
       cards: cards,
+      newFront: '',
+      newBack: '',
+      error: false
     },
     methods: {
-
+      toggleCard: function(card){
+        card.flipped = !card.flipped;
+      },
+      addNew: function(){
+        if(!this.newFront || !this.newBack) {
+          this.error = true
+        } else {
+          this.cards.push({
+            front: this.newFront,
+            back: this.newBack,
+            flipped: false
+          })
+          this.newFront = '';
+          this.newBack = '';
+          this.error = false;
+        }
+      }
     }
   });
